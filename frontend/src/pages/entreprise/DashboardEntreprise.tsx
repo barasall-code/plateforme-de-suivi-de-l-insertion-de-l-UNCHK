caac6590dafd03e3f88dbe501f6bc540c30303b1
Barasall <bara.sall@unchk.edu.sn>
2026-03-18 11:58:48 +0000

    fix: import Notifications manquant dans DashboardEntreprise + navbar bleue

--- frontend/src/pages/entreprise/DashboardEntreprise.tsx
+++ frontend/src/pages/entreprise/DashboardEntreprise.tsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 import api from '../../services/api';
 import BadgeMessages from '../../components/BadgeMessages';
+import Notifications from '../../components/Notifications';
 
 interface Offre {
   id: string;
